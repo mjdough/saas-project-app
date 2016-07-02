396f15f32b87a9408788a622e43203e76526b8fe
mjdough <doughmj@outlook.com>
2016-07-02 21:24:31 +0000

    Add artifacts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,7 @@ ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_
     Tenant.find(tenant_id).name
   end
 
+  def s3_link(tenant_id, artifact_key)
+    link_to artifact_key, "#{artifact_key}", class: "main-link", target: 'new'
+  end
 end
