5e36fde6521cc7cacc1fb3f2c05f8a85a79f4c6f
mjdough <doughmj@outlook.com>
2016-08-05 12:17:04 +0000

    Changes to artifacts

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -29,9 +29,7 @@ class ArtifactsController < ApplicationController
 
     respond_to do |format|
       if @artifact.save
-        format.html { redirect_to tenant_project_url(tenant_id:
-          Tenant.current_tenant_id, id: @artifact.project_id),
-                  notice: 'Artifact was successfully created.' }
+        format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @artifact.project_id), notice: 'Artifact was successfully created.' }
         format.json { render :show, status: :created, location: @artifact }
       else
         format.html { render :new }
@@ -45,8 +43,7 @@ class ArtifactsController < ApplicationController
   def update
     respond_to do |format|
       if @artifact.update(artifact_params)
-        format.html { redirect_to @artifact,
-                notice: 'Artifact was successfully updated.' }
+        format.html { redirect_to @artifact, notice: 'Artifact was successfully updated.' }
         format.json { render :show, status: :ok, location: @artifact }
       else
         format.html { render :edit }
@@ -60,9 +57,7 @@ class ArtifactsController < ApplicationController
   def destroy
     @artifact.destroy
     respond_to do |format|
-      format.html { redirect_to tenant_project_url(tenant_id:
-            Tenant.current_tenant_id, id: @artifact.project_id),
-                  notice: 'Artifact was successfully destroyed.' }
+      format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @artifact.project_id), notice: 'Artifact was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -4,7 +4,7 @@ class Artifact < ActiveRecord::Base
   attr_accessor :upload
   belongs_to :project
 
-  MAX_FILESIZE = 10.megabytes
+  MAX_FILESIZE = 20.megabytes
 
   validates_presence_of :name, :upload
   validates_uniqueness_of :name
@@ -23,9 +23,8 @@ class Artifact < ActiveRecord::Base
 
   def uploaded_file_size
       if upload
-        errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}")
-                          unless upload.size <= self.class::MAX_FILESIZE
+        errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= (self.class::MAX_FILESIZE)
       end
   end
-end
+
 end
